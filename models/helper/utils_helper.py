7796674ff2bb16b59b3da8fc5c57e81a2a400a9d
JingVIPLab <csjzhang806@gmail.com>
2025-02-21 14:23:09 +0800

    Create utils_helper.py

---
+++ models/helper/utils_helper.py
@@ -0,0 +1,46 @@
+import torch
+import numpy as np
+import time
+
+
+class Averager():
+    def __init__(self):
+        self.n = 0
+        self.v = 0
+
+    def add(self, x):
+        self.v = (self.v * self.n + x) / (self.n + 1)
+        self.n += 1
+
+    def item(self):
+        return self.v
+
+
+def count_acc(logits, label):
+    pred = torch.argmax(logits, dim=1)
+    if torch.cuda.is_available():
+        return (pred == label).type(torch.cuda.FloatTensor).mean().item()
+    else:
+        return (pred == label).type(torch.FloatTensor).mean().item()
+
+
+def compute_confidence_interval(data):
+    a = 1.0 * np.array(data)
+    m = np.mean(a)
+    std = np.std(a)
+    pm = 1.96 * (std / np.sqrt(len(a)))
+    return m, pm
+
+
+class Timer():
+    def __init__(self):
+        self.o = time.time()
+
+    def measure(self, p=1):
+        x = (time.time() - self.o) / p
+        x = int(x)
+        if x >= 3600:
+            return '{:.1f}h'.format(x / 3600)
+        if x >= 60:
+            return '{}m'.format(round(x / 60))
+        return '{}s'.format(x)
